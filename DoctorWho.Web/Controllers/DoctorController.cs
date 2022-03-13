c91d2bde182e1204df59d9cb11b1f17ece2e23dc
Lujain Abusada <Lujain.Abusada@gmail.com>
2022-03-13 13:16:18 +0200

    Create a controller for the doctors with a Get all doctors API.

---
+++ DoctorWho.Web/Controllers/DoctorController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using DoctorWho.Db.DataModels;
+using DoctorWho.Db.Repositories;
+using DoctorWho.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DoctorWho.Web.Controllers
+{
+    [ApiController]
+    [Route("api/doctors")]
+
+    public class DoctorController : ControllerBase
+    {
+        private readonly DoctorRepository _doctorRepository;
+        private readonly IMapper _mapper;
+
+        public DoctorController(IMapper mapper)
+        {
+            _doctorRepository = new DoctorRepository();
+            _mapper = mapper;
+        }
+
+        [HttpGet()]
+        public ActionResult<IEnumerable<Doctor>> GetDoctors()
+        {
+            var doctorsFromRepo = _doctorRepository.GetDoctors();
+            return Ok(_mapper.Map<List<DoctorDto>>(doctorsFromRepo));
+        }
+
+        [HttpGet("{doctorId}", Name = "GetDoctor")]
+        public ActionResult<IEnumerable<Doctor>> GetDoctor(int doctorId)
+        {
+            if (_doctorRepository.GetDoctors().Where(d => d.DoctorId == doctorId).FirstOrDefault() == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var doctorFromRepo = _doctorRepository.GetDoctors().Where(d => d.DoctorId == doctorId).FirstOrDefault();
+                return Ok(_mapper.Map<DoctorDto>(doctorFromRepo));
+            }
+        }
+    }
+}
