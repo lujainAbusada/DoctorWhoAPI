f053b5c9a4e033279f08ee2dcce020073f4af90a
Lujain Abusada <Lujain.Abusada@gmail.com>
2022-03-13 13:43:33 +0200

    Add AddCompanionToEpisode API

---
+++ DoctorWho.Web/Models/CompanionForCreationDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoctorWho.Web.Models
+{
+    public class CompanionForCreationDto
+    {
+        public string CompanionName { get; set; }
+        public string WhoPlayed { get; set; }
+    }
+}
---
+++ DoctorWho.Web/Profiles/CompanionProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using DoctorWho.Db.DataModels;
+using DoctorWho.Web.Models;
+
+
+namespace DoctorWho.Web.Profiles
+{
+    public class CompanionProfile : Profile
+    {
+        public CompanionProfile()
+        {
+            CreateMap<Companion, CompanionDto>();
+            CreateMap<CompanionForCreationDto, Companion>();
+        }
+    }
+}
+
