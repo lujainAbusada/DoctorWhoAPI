c73836ec64ebfaa618db3fdb7061ec4ffc0d4aeb
Lujain Abusada <Lujain.Abusada@gmail.com>
2022-03-13 13:39:41 +0200

    Add AddEnemyToEpisode API

---
+++ DoctorWho.Web/Controllers/EnemyController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using DoctorWho.Db.Repositories;
+using DoctorWho.Db.DataModels;
+using DoctorWho.Web.Models;
+using AutoMapper;
+
+namespace DoctorWho.Web.Controllers
+{
+    [ApiController]
+    public class EnemyController : ControllerBase
+    {
+        private readonly EpisodeRepository _episodeRepository;
+        private readonly EnemyRepository _enemyRepository;
+        private readonly IMapper _mapper;
+
+        public EnemyController(IMapper mapper)
+        {
+            _episodeRepository = new EpisodeRepository();
+            _enemyRepository = new EnemyRepository();
+            _mapper = mapper;
+        }
+
+        [HttpGet("api/enemies")]
+        public ActionResult<IEnumerable<Enemy>> GetEnemies()
+        {
+            var enemiesFromRepo = _enemyRepository.GetEnemies();
+            return Ok(_mapper.Map<List<EnemyDto>>(enemiesFromRepo));
+        }
+
+        [HttpPost("api/episodes/{episodeId}/enemies")]
+        public ActionResult<EpisodeDto> AddEnemyToEpisode(EnemyForCreationDto enemy, int episodeId)
+        {
+            var newEnemy = _mapper.Map<Enemy>(enemy);
+            _enemyRepository.InsertEnemy(newEnemy);
+            _episodeRepository.AddEnemy(_episodeRepository.GetEpisodes().Where(e => e.EpisodeId == episodeId).FirstOrDefault(), newEnemy.EnemyId);
+            return Ok("Enemy Added To Episode");
+        }
+    }
+}
---
+++ DoctorWho.Web/Models/EnemyDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoctorWho.Web.Models
+{
+    public class EnemyDto
+    {
+        public int EnemyId { get; set; }
+        public string EnemyName { get; set; }
+        public string Description { get; set; }
+    }
+}
