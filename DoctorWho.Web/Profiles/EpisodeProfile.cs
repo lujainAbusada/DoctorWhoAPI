97038df44d17b640cd42d777664cc10c673254d8
Lujain Abusada <Lujain.Abusada@gmail.com>
2022-03-13 13:31:39 +0200

    Create a controller for the episodes with a Get all episodes API

---
+++ DoctorWho.Web/Profiles/EpisodeProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DoctorWho.Db.DataModels;
+using DoctorWho.Web.Models;
+
+namespace DoctorWho.Web.Profiles
+{
+    public class EpisodeProfile : Profile
+    {
+        public EpisodeProfile()
+        {
+            CreateMap<Episode, EpisodeDto>();
+        }
+    }
+}
